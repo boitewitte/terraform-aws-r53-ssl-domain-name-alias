ced9fe7fe8c1e6b25d2abbaf0e258b2a49f104e1
Boite Witte <info@boite.nl>
2019-03-24 15:12:31 +0100

    Fixed amount of aws_route53_Records

--- main.tf
+++ main.tf
@@ -57,7 +57,7 @@ resource "aws_route53_record" "cert_validation" {
 }
 
 resource "aws_acm_certificate_validation" "cert" {
-  count = "${local.enabled == "true" ? length(aws_acm_certificate.cert.domain_validation_options) : 0}"
+  count = "${local.enabled == "true" ? 1 : 0}"
 
   certificate_arn = "${aws_acm_certificate.cert.arn}"
 
