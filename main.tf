aa33afd6f7856f88d4929d469563bd4e46dd402d
Boite Witte <info@boite.nl>
2019-03-24 16:02:17 +0100

    1 item is string

--- main.tf
+++ main.tf
@@ -61,7 +61,7 @@ resource "aws_acm_certificate_validation" "cert" {
 
   certificate_arn = "${aws_acm_certificate.cert.arn}"
 
-  validation_record_fqdns = ["${aws_route53_record.cert_validation.fqdn}"]
+  validation_record_fqdns = ["${element(aws_route53_record.cert_validation.*.fqdn, count.index)}"]
 }
 
 resource "aws_route53_record" "service" {
